53c59718d698618497209bf5828b62e46a3809f1
Amita Patel <amitap16@live.com>
2021-06-09 22:44:11 -0400

    Refactor Counter & User service logic.

--- src/app/shared/services/counter.service.ts
+++ src/app/shared/services/counter.service.ts
@@ -2,18 +2,16 @@ import { Injectable } from "@angular/core";
 
 @Injectable({ providedIn: 'root' })
 export class CounterService {
-  private activeCounter: number = 0;
-  private inactiveCounter: number = 0;
+  private activeToInactiveCounter: number = 0;
+  private inactiveToActiveCounter: number = 0;
 
-  addActiveCounter() {
-    this.activeCounter++;
-    console.log('In CounterService class - Inactive to Active: ' + this.activeCounter);
-    return this.activeCounter;
+  IncrementActiveToInactive() {
+    this.activeToInactiveCounter++;
+    console.log('Active to Inactive: ' + this.activeToInactiveCounter);
   }
 
-  addInactiveCounter() {
-    this.inactiveCounter++;
-    console.log('In CounterService class - Active to Inactive: ' + this.inactiveCounter);
-    return this.inactiveCounter;
+  IncrementInactiveToActive() {
+    this.inactiveToActiveCounter++;
+    console.log('Inactive to Active: ' + this.inactiveToActiveCounter);
   }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -6,22 +6,17 @@ export class UserService {
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
 
-  activeCounter: number;
-  inactiveCounter: number;
-
   constructor(private counterService: CounterService) { }
 
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
-    this.activeCounter = this.counterService.addInactiveCounter();
-    console.log('In UserService class - Active to Inactive: ' + this.activeCounter);
+    this.counterService.IncrementActiveToInactive();
   }
 
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
-    this.inactiveCounter = this.counterService.addActiveCounter();
-    console.log('In UserService class - Inactive to Active: ' + this.inactiveCounter);
+    this.counterService.IncrementInactiveToActive();
   }
 }
